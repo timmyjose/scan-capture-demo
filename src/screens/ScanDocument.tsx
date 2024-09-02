9402384d12b6ed2a444a0c9692656d349e0740eb
Timmy Jose <zoltan.jose@gmail.com>
2024-09-02 17:44:27 +0530

    feat: basic document scanning working.

--- src/screens/ScanDocument.tsx
+++ src/screens/ScanDocument.tsx
@@ -1,14 +1,39 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../App'
+import { useState } from 'react'
+import DocumentScanner from 'react-native-document-scanner-plugin'
 
 const ScanDocument = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
+  const [scannedImage, setScannedImage] = useState<string | null>(null)
+
+  const scanImage = async () => {
+    const { scannedImages } = await DocumentScanner.scanDocument()
+
+    if (scannedImages && scannedImages.length > 0) {
+      console.log(`scannedImages = ${scannedImages}`)
+      setScannedImage(scannedImages[0])
+    }
+  }
+
   return (
     <View style={styles.container}>
+      { scannedImage && (
+        <Image
+          resizeMode="contain"
+          key={scannedImage}
+          style={{ width: 300, height: 300 }}
+          source={{ uri: scannedImage }}
+        />
+      )}
+      <Pressable
+        style={styles.button}
+        onPress={scanImage}>
+          <Text>Scan</Text>
+        </Pressable>
       <Pressable
         style={styles.button}
         onPress={() => navigation.navigate('Home')}>
